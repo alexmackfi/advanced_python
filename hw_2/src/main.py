804a426c7722462278e916f8fa6370dcb0cce5e2
Emilia Bojarskaja <alexmackgrwr@gmail.com>
2024-03-25 15:11:45 +0300

    update main code

--- hw_2/src/main.py
+++ hw_2/src/main.py
@@ -1,8 +1,10 @@
-import hw2
+import latex_package_alexmackfi as hw2
 
+hw2.start_document()
 hw2.drow_table([['Meal',"Time","Table"],
             ["Milk","Morning","arround"],
             ["Bread","Dinner","square"]
             ])
 
 hw2.drow_image('nerpa.jpg')
+hw2.finish_document()
